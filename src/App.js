994dc0fd4a769c863159f36f6b4252b3e7f24efe
tbrincatos <tamsinbrincatos@gmail.com>
2022-07-26 21:45:28 +0200

    Added footer and styled it

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <SearchEngine />
+        <br />
+        <footer>
+          <a
+            href="https://github.com/tbrincatos/weather-hw"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced code
+          </a>{" "}
+          coded by Tamsin Brincat👩🏻‍🦰
+        </footer>
       </header>
     </div>
   );
